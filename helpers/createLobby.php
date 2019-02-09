3e1cbbcef0fa896a86a732cc9bd9f8cc7851c1f9
Cuzzo01 <Nick@Nicks-Macbook.local>
2019-02-09 03:27:23 -0600

    Dude idk, its kinda working now, I did some stuff

--- helpers/createLobby.php
+++ helpers/createLobby.php
@@ -38,7 +38,7 @@
               ID INT(32) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
               username VARCHAR(15) NOT NULL,
               coins int(32) NOT NULL DEFAULT '1000',
-              bet int(32) NOT NULL,
+              bet int(32) NOT NULL DEFAULT 0,
               reg_date TIMESTAMP
             )";
   $result = mysqli_query($conn, $query) or die(mysqli_error($conn));;
