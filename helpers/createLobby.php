be90826cc6bfe0b730468ae5dc790bd0eba33155
Nick Marcuzzo <Nick@Nicks-Macbook.local>
2019-02-09 01:40:56 -0600

    Maybe actually finished createLobby, started and maybe finished joinLobby

--- helpers/createLobby.php
+++ helpers/createLobby.php
@@ -15,7 +15,6 @@
     $conn->select_db("settings");
 
     $result = mysqli_query($conn, "SELECT * FROM gameCodes WHERE code = '$code'");
-    echo $conn->error;
   	$count = mysqli_num_rows($result);
     if ($count != 0) {
       return(false);
@@ -31,5 +30,38 @@
   }
 
   $result = mysqli_query($conn, "INSERT INTO gameCodes (code) VALUES ('$gameCode')");
+
+
+  $result = mysqli_query($conn, "CREATE DATABASE game_$gameCode");
+  $conn->select_db("game_$gameCode");
+  $query = "CREATE TABLE users (
+              ID INT(32) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+              username VARCHAR(15) NOT NULL,
+              coins int(32) NOT NULL DEFAULT '1000',
+              bet int(32) NOT NULL,
+              reg_date TIMESTAMP
+            )";
+  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));;
+  $query = "CREATE TABLE unusedCards (
+              ID INT(32) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+              card VARCHAR(2) NOT NULL
+            )";
+  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));;
+  $query = "INSERT INTO unusedCards
+                (card)
+            VALUES
+                ('2S'), ('3S'), ('4S'), ('5S'), ('6S'), ('7S'), ('8S'), ('9S'), ('1S'), ('JS'), ('QS'), ('KS'), ('AS'),
+                ('2C'), ('3C'), ('4C'), ('5C'), ('6C'), ('7C'), ('8C'), ('9C'), ('1C'), ('JC'), ('QC'), ('KC'), ('AC'),
+                ('2H'), ('3H'), ('4H'), ('5H'), ('6H'), ('7H'), ('8H'), ('9H'), ('1H'), ('JH'), ('QH'), ('KH'), ('AH'),
+                ('2D'), ('3D'), ('4D'), ('5D'), ('6D'), ('7D'), ('8D'), ('9D'), ('1D'), ('JD'), ('QD'), ('KD'), ('AD')
+                ";
+  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));;
+  $query = "CREATE TABLE dealerHand (
+              ID INT(32) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+              card VARCHAR(2) NOT NULL
+            )";
+  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));;
+  //header("Location: joinLobby.php?code=$gameCode");
+
   $conn->close();
 ?>
---
+++ helpers/joinLobby.php
@@ -0,0 +1,33 @@
+<?php
+  include 'mysqlLogin.php';
+
+  $gameCode = $_GET["gameCode"];
+  $conn->select_db("settings");
+  $result = mysqli_query($conn, "SELECT * FROM gameCodes WHERE code = '$gameCode'");
+  $count = mysqli_num_rows($result);
+  if ($count == 0) {
+    header("Location: ../join.html?err=1");
+  }
+
+  if (!isset($_SESSION)) {
+    session_start();
+  }
+  $_SESSION['gameCode'] = $_GET["gameCode"];
+  $_SESSION['userName'] = $_GET["userName"];
+  $gameCode = $_SESSION['gameCode'];
+  $username = $_SESSION['userName'];
+  $conn->select_db("game_$gameCode");
+
+  $query = "INSERT INTO users (username) VALUES ('$username')";
+  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  $playerID = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'"))['ID'];
+
+  $query = "CREATE TABLE " . $playerID . "hand (
+              ID INT(32) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+              card VARCHAR(2) NOT NULL
+            )";
+  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+  header("Location: ../lobby.html");
+  $conn->close();
+?>
