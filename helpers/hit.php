0f1172a80e95de2403e167cd08b8674f2b50c540
Cuzzo01 <nicholas.marcuzzo@gmail.com>
2019-02-09 15:21:00 -0600

    Hit and stay now work, buttons only show up when they should

--- helpers/hit.php
+++ helpers/hit.php
@@ -3,5 +3,16 @@
   if (!isset($_SESSION)) {
     session_start();
   }
-  
+  $username = $_SESSION['username'];
+  $gameCode = $_SESSION['gameCode'];
+  $conn->select_db("game_$gameCode");
+  include 'deal.php';
+  $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'") or die(mysqli_error($conn));
+  $row = mysqli_fetch_array($result);
+  if ($row['nextAction'] != 'userplay') {
+    header("Location: ../game.php");
+  } else {
+    deal($conn, $row['ID'], 1);
+    header("Location: ../game.php");
+  }
 ?>
