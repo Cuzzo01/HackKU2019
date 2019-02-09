7d6fd5eb2bae8e355d14e57f97f2ca4507e7813c
Cameron Kientz <noremackientz@gmail.com>
2019-02-09 17:03:45 -0600

    added waiting screen

---
+++ waiting.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../css/waiting.css">
+
+    <script type="text/javascript" src="../js/script.js"></script>
+
+    <title>Card Game</title>
+</head>
+<body>
+  <div class="container-fluid" id="header">
+      CardSimulator
+  </div>
+  <div class="container-fluid" id="waitingMsg">
+    Waiting for players to place their bets...
+    <div class="sk-folding-cube">
+    <div class="sk-cube1 sk-cube"></div>
+    <div class="sk-cube2 sk-cube"></div>
+    <div class="sk-cube4 sk-cube"></div>
+    <div class="sk-cube3 sk-cube"></div>
+  </div>
+</div>
+</body>
