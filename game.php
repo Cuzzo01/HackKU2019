6a55c4b4c9bbaca207b4fdcc905607c00d879c8b
Cameron Kientz <noremackientz@gmail.com>
2019-02-09 23:04:25 -0600

    fuck

--- game.php
+++ game.php
@@ -41,25 +41,25 @@
         <button type="button" class="btn btn-primary" id="menu">Menu</button>
     </div>
     <div class="container-fluid" id="otherPlayers">
-      <div class="container-fluid player">
-        <label class="container-fluid" id="playerName">
-          Player1
-        </label>
-        <div class="container-fluid" id="status"></div>
-        <div class="container-fluid" id="playerInfo">
-          <div class="container-fluid" id="coinCount">Coins:</div>
-          <div class="container-fluid" id="bet">Bet:</div>
-        </div>
-        <div class="container pile pile3">
-        <?php
-          $result = mysqli_query($conn, "SELECT * FROM dealerHand") or die(mysqli_error($conn));
-          while ($row = mysqli_fetch_array($result)) {
-            echo "<image class='card' src='../CardCropped/" . strtolower($row['card']) . ".png'>";
+      <?php
+        $conn->select_db("game_$gameCode");
+        $result = mysqli_query($conn, "SELECT * FROM users") or die(mysqli_error($conn));
+        while ($row = mysqli_fetch_array($result)) {
+          if ($row['username'] == $_SESSION['username']) {
+            continue;
+          } else {
+            echo "<div class='container-fluid player'><label class='container-fluid' id='playerName'>";
+            echo $row['username'];
+            echo "</label><div class="container-fluid" id="status"></div><div class="container-fluid" id="playerInfo"><div class="container-fluid" id="coinCount">Coins:</div><div class="container-fluid" id="bet">Bet:</div></div><div class='container pile pile3'>";
+            $tableName = $row['ID'] . 'hand';
+            $result2 = mysqli_query($conn, "SELECT * FROM $tableName") or die(mysqli_error($conn));
+            while ($row = mysqli_fetch_array($result2)) {
+              echo "<image class='card' src='../CardCropped/" . strtolower($row['card']) . ".png'>";
+            }
+            echo "</div></div>";
           }
-        ?>
-        </div>
-      </div>
-    </div>
+        }
+      ?>
     <div class="container-fluid" id="board">
       <label class="container-fluid" id="playerName">
         Board/Dealer
